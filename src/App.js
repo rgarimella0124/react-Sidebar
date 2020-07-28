0ef01dd3b101b36150b39e48d9bc1614abd08a51
Surya <ram@neokred.tech>
2020-07-28 11:44:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import SideBar from "./components/sidebar/SideBar";
+import Content from "./components/content/Content";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // Moblie first
+    this.state = {
+      isOpen: false,
+      isMobile: true
+    };
+
+    this.previousWidth = -1;
+  }
+
+  updateWidth() {
+    const width = window.innerWidth;
+    const widthLimit = 576;
+    const isMobile = width <= widthLimit;
+    const wasMobile = this.previousWidth <= widthLimit;
+
+    if (isMobile !== wasMobile) {
+      this.setState({
+        isOpen: !isMobile
+      });
+    }
+
+    this.previousWidth = width;
+  }
+
+  /**
+   * Add event listener
+   */
+  componentDidMount() {
+    this.updateWidth();
+    window.addEventListener("resize", this.updateWidth.bind(this));
+  }
+
+  /**
+   * Remove event listener
+   */
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateWidth.bind(this));
+  }
+
+  toggle = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
+
+  render() {
+    return (
+      <div className="App wrapper">
+        <SideBar toggle={this.toggle} isOpen={this.state.isOpen} />
+        <Content toggle={this.toggle} isOpen={this.state.isOpen} />
+      </div>
+    );
+  }
 }
 
 export default App;
